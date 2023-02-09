b89d7ccdebb1459c36bcd5b271f00dd8085a4c8b
Alexander Holder <drowish95z@gmail.com>
2023-02-09 11:11:07 -0600

    Exercises Complete with Console Log

--- app.js
+++ app.js
@@ -3,8 +3,37 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 & 2
+//Combined Declaration + Assignment
+const firstName = "Alex";
+let lastName = "Holder";
+var age = 27;
 
 // Exercise 2
+//Reassignment for Exercise Purposes
+lastName = "Holder";
+age = 27;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercse 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+//Test
+console.Log(firstBame + " " + lastName + " " + age + " " + language + " " + createdYear + " " + isCaseSensitive + " " + price + " " + isOnSale + " " + salePercentage);
+console.Log(stock + " " + inStock + " " + selectedSize + " " + title + " " + author + " " + pageCount + " " + bookmark + " " + hasRead)
